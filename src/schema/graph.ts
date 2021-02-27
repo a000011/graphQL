d889591127c7b5dad70b3be30380f620fb6139c3
антон надькин <anton.nadikin@gmail.com>
2021-02-27 13:29:41 +0300

    дошел до бд

--- src/schema/graph.ts
+++ src/schema/graph.ts
@@ -1,9 +1,20 @@
-import { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList } from "graphql";
+
+class Std{
+    id: string;
+    name: string;
+    sex: string;
+    groupid: string;
+}
 
 let students = [
     { id: "1", name: "Anton", sex: "man", groupid: "1" },
     { id: "2", name: "Sasha", sex: "woman", groupid: "2" },
-    { id: "3", name: "Ales", sex: "woman", groupid: "2" }
+    { id: "3", name: "Anton", sex: "man", groupid: "1" },
+    { id: "4", name: "l", sex: "man", groupid: "2" },
+    { id: "5", name: "Лена", sex: "ж", groupid: "1" },
+    { id: "6", name: "кеннатий", sex: "man", groupid: "2" },
+    { id: "7", name: "Ales", sex: "woman", groupid: "2" }
 ];
 
 let groups = [
@@ -11,21 +22,41 @@ let groups = [
     { id: "2", name: "gk-11" }
 ];
 
-const StudentType = new GraphQLObjectType({
+const StudentType: GraphQLObjectType = new GraphQLObjectType({
     name: "Student",
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         sex: { type: GraphQLString },
-        groupid: { type: GraphQLID }
+        groupid: { type: GraphQLID },
+        group:{
+            type: GroupType,
+            resolve(parent, args){
+                let a;
+                groups.forEach(el => {
+                    if (el.id == parent.groupid) { a = el };
+                });
+                return a;
+            }
+        }
     })
 })
 
-const GroupType = new GraphQLObjectType({
+const GroupType: GraphQLObjectType = new GraphQLObjectType({
     name: "Group",
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        sudents:{
+            type: new GraphQLList(StudentType),
+            resolve(parent: any, args: any){
+                let a: Array<Std> = [];
+                students.forEach(el => {
+                    if (el.groupid == parent.id) { a.push(el) };
+                });
+                return a;
+            }
+        }
     })
 })
 
@@ -63,6 +94,12 @@ const RootQuery = new GraphQLObjectType({
                 return b;
             }
 
+        },
+        Students:{
+            type: new GraphQLList(StudentType),
+            resolve(parent, args){
+                return students;
+            }
         }
     }
 })
